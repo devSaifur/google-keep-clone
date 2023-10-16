07452b22f815d21e3ac8753664a52527ea9558ae
devSaifur <saifur2akash@gmail.com>
2023-10-16 17:19:04 +0600

    firebase deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { lazy } from 'react'
 import AppContainer from './ui/AppContainer'
-import Header from './ui/Header/Header'
-import Main from './ui/Main'
-import AddNote from './features/Note/components/AddNote'
-import Notes from './features/Note/components/Notes'
-import MadeWith from './ui/MadeWith'
+
+const Header = lazy(() => import('./ui/Header/Header'))
+const Main = lazy(() => import('./ui/Main'))
+const AddNote = lazy(() => import('./features/Note/components/AddNote'))
+const Notes = lazy(() => import('./features/Note/components/Notes'))
+const MadeWith = lazy(() => import('./ui/MadeWith'))
 
 function App() {
   return (
